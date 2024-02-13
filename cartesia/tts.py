af91bce97cdfd92548c6fe994639e42b61c0ccbe
Arjun Desai <arjunaashna@gmail.com>
2024-02-13 23:48:07 +0000

    add initial tts client

---
+++ cartesia/tts.py
@@ -0,0 +1,89 @@
+import base64
+import json
+import os
+from typing import List
+
+import numpy as np
+import requests
+
+DEFAULT_MODEL_ID = "echo_tts_v0.0.6"
+
+DEFAULT_BASE_URL = "api.cartesia.ai"
+
+
+class CartesiaTTS:
+    """The client for Cartesia's text-to-speech library."""
+
+    def __init__(self, *, model_id: str = None, api_key: str = None):
+        """
+        Args:
+            api_key: The API key to use for authorization.
+        """
+        self.model_id = model_id or DEFAULT_MODEL_ID
+        self.base_url = os.environ.get("CARTESIA_BASE_URL", DEFAULT_BASE_URL)
+
+    def models(self) -> List[str]:
+        """Get a list of available models."""
+        raise NotImplementedError()
+
+    def voices(self, model_id: str = None) -> List[str]:
+        """Returns a list of voices for a given model."""
+        model_id = model_id or self.model_id
+        response = requests.get(f"{self._http_url()}/models/{model_id}/voices")
+
+        if response.status_code != 200:
+            raise ValueError(f"Failed to get voices for model {model_id}. {response.text}")
+
+        return json.loads(response.text)
+
+    def generate(
+        self,
+        *,
+        model_id: str = None,
+        transcript: str,
+        duration: int = None,
+        chunk_time: float = None,
+        lookahead: int = None,
+        voice: str = None,
+        stream: bool = False,
+    ):
+        body = dict(
+            transcript=transcript,
+            model_id=model_id or DEFAULT_MODEL_ID,
+        )
+        optional_body = dict(
+            duration=duration,
+            chunk_time=chunk_time,
+            lookahead=lookahead,
+            voice=voice,
+        )
+        body.update({k: v for k, v in optional_body.items() if v is not None})
+
+        response = requests.post(f"{self._http_url()}/stream", stream=True, data=json.dumps(body))
+        if response.status_code != 200:
+            raise ValueError(f"Failed to generate audio. {response.text}")
+
+        def generator():
+            for chunk in response.iter_content(chunk_size=None):
+                # Decode the JSON-encoded bytes to a dictionary
+                data_dict = json.loads(chunk.decode("utf-8"))
+                data = base64.b64decode(data_dict["data"])
+                audio = np.frombuffer(data, dtype=np.float32)
+
+                yield {"audio": audio, "sampling_rate": data_dict["sampling_rate"]}
+
+        if stream:
+            return generator()
+
+        chunks = []
+        sampling_rate = None
+        for chunk in generator():
+            if sampling_rate is None:
+                sampling_rate = chunk["sampling_rate"]
+            chunks.append(chunk["audio"])
+
+        return {"audio": np.concatenate(chunks), "sampling_rate": sampling_rate}
+
+    def _http_url(self):
+        prefix = "http" if "localhost" in self.base_url else "https"
+        return f"{prefix}://{self.base_url}"
---
+++ setup.py
@@ -0,0 +1,253 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import io
+import os
+
+# Note: To use the 'upload' functionality of this file, you must:
+#   $ pipenv install twine --dev
+import shutil
+import subprocess
+import sys
+from distutils.util import convert_path
+from shutil import rmtree
+
+from setuptools import Command, find_packages, setup
+
+PACKAGE_DIR = "cartesia"
+main_ns = {}
+ver_path = convert_path(os.path.join(PACKAGE_DIR, "version.py"))
+with open(ver_path) as ver_file:
+    exec(ver_file.read(), main_ns)
+
+
+# Package meta-data.
+NAME = "cartesia"
+DESCRIPTION = "The official Python library for the Cartesia API."
+URL = ""
+EMAIL = "arjun@cartesia.ai"
+AUTHOR = "Cartesia, Inc."
+REQUIRES_PYTHON = ">=3.9.0"
+VERSION = main_ns["__version__"]
+
+
+# What packages are required for this module to be executed?
+def get_requirements(path):
+    with open(path, "r") as f:
+        out = f.read().splitlines()
+
+    out = [line.strip() for line in out]
+    return out
+
+
+REQUIRED = get_requirements("requirements.txt")
+
+# What packages are optional?
+EXTRAS = {}
+
+# The rest you shouldn't have to touch too much :)
+# ------------------------------------------------
+# Except, perhaps the License and Trove Classifiers!
+# If you do change the License, remember to change the Trove Classifier for that!
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+# Import the README and use it as the long-description.
+# Note: this will only work if 'README.md' is present in your MANIFEST.in file!
+try:
+    with io.open(os.path.join(here, "README.md"), encoding="utf-8") as f:
+        long_description = "\n" + f.read()
+except FileNotFoundError:
+    long_description = DESCRIPTION
+
+# Load the package's __version__.py module as a dictionary.
+about = {}
+if not VERSION:
+    project_slug = NAME.lower().replace("-", "_").replace(" ", "_")
+    with open(os.path.join(here, project_slug, "__version__.py")) as f:
+        exec(f.read(), about)
+else:
+    about["__version__"] = VERSION
+
+
+class UploadCommand(Command):
+    """Support setup.py upload.
+    Adapted from https://github.com/HazyResearch/meerkat.
+    """
+
+    description = "Build and publish the package."
+    user_options = []
+
+    @staticmethod
+    def status(s):
+        """Prints things in bold."""
+        print("\033[1m{0}\033[0m".format(s))
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        try:
+            self.status("Removing previous builds…")
+            rmtree(os.path.join(here, "dist"))
+        except OSError:
+            pass
+
+        self.status("Building Source and Wheel (universal) distribution…")
+        os.system("{0} setup.py sdist bdist_wheel --universal".format(sys.executable))
+
+        self.status("Uploading the package to PyPI via Twine…")
+        os.system("twine upload dist/*")
+
+        self.status("Pushing git tags…")
+        os.system("git tag v{0}".format(about["__version__"]))
+        os.system("git push --tags")
+
+        sys.exit()
+
+
+class BumpVersionCommand(Command):
+    """
+    To use: python setup.py bumpversion -v <version>
+
+    This command will push the new version directly and tag it.
+    """
+
+    description = "Installs the foo."
+    user_options = [
+        ("version=", "v", "the new version number"),
+    ]
+
+    @staticmethod
+    def status(s):
+        """Prints things in bold."""
+        print("\033[1m{0}\033[0m".format(s))
+
+    def initialize_options(self):
+        self.version = None
+
+    def finalize_options(self):
+        # This package cannot be imported at top level because it
+        # is not recognized by Github Actions.
+        from packaging import version
+
+        if self.version is None:
+            raise ValueError("Please specify a version number.")
+
+        current_version = about["__version__"]
+        if not version.Version(self.version) > version.Version(current_version):
+            raise ValueError(
+                f"New version ({self.version}) must be greater than "
+                f"current version ({current_version})."
+            )
+
+    def _undo(self):
+        os.system(f"git restore --staged {PACKAGE_DIR}/__init__.py")
+        os.system(f"git checkout -- {PACKAGE_DIR}/__init__.py")
+
+    def run(self):
+        current_version = about["__version__"]
+
+        self.status("Checking current branch is 'main'")
+        current_branch = get_git_branch()
+        if current_branch != "main":
+            raise RuntimeError(
+                "You can only bump the version from the 'main' branch. "
+                "You are currently on the '{}' branch.".format(current_branch)
+            )
+
+        self.status("Pulling latest changes from origin")
+        err_code = os.system("git pull")
+        if err_code != 0:
+            raise RuntimeError("Failed to pull from origin.")
+
+        self.status("Checking working directory is clean")
+        err_code = os.system("git diff --exit-code")
+        err_code += os.system("git diff --cached --exit-code")
+        if err_code != 0:
+            raise RuntimeError("Working directory is not clean.")
+
+        # TODO: Add check to see if all tests are passing on main.
+
+        # Change the version in __init__.py
+        self.status(f"Updating version {current_version} -> {self.version}")
+        update_version(self.version)
+        if current_version != self.version:
+            self._undo()
+            raise RuntimeError("Failed to update version.")
+
+        self.status(f"Adding {PACKAGE_DIR}/__init__.py to git")
+        err_code = os.system(f"git add {PACKAGE_DIR}/__init__.py")
+        if err_code != 0:
+            self._undo()
+            raise RuntimeError("Failed to add file to git.")
+
+        # Commit the file with a message '[bumpversion] v<version>'.
+        self.status(f"Commit with message '[bumpversion] v{self.version}'")
+        err_code = os.system("git commit -m '[bumpversion] v{}'".format(current_version))
+        if err_code != 0:
+            self._undo()
+            raise RuntimeError("Failed to commit file to git.")
+
+        # Push the commit to origin.
+        # self.status("Pushing commit to origin")
+        # err_code = os.system("git push")
+        # if err_code != 0:
+        #     # TODO: undo the commit automatically.
+        #     raise RuntimeError("Failed to push commit to origin.")
+
+        sys.exit()
+
+
+def update_version(version):
+    import json
+
+    # Update python.
+    init_py = [
+        line if not line.startswith("__version__") else f'__version__ = "{version}"\n'
+        for line in open(ver_path, "r").readlines()
+    ]
+    with open(ver_path, "w") as f:
+        f.writelines(init_py)
+
+
+def get_git_branch():
+    """Return the name of the current branch."""
+    proc = subprocess.Popen(["git branch"], stdout=subprocess.PIPE, shell=True)
+    (out, err) = proc.communicate()
+    if err is not None:
+        raise RuntimeError(f"Error finding git branch: {err}")
+    out = out.decode("utf-8").split("\n")
+    current_branch = [line for line in out if line.startswith("*")][0]
+    current_branch = current_branch.replace("*", "").strip()
+    return current_branch
+
+
+# Where the magic happens:
+setup(
+    name=NAME,
+    version=about["__version__"],
+    description=DESCRIPTION,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author=AUTHOR,
+    author_email=EMAIL,
+    python_requires=REQUIRES_PYTHON,
+    url=URL,
+    packages=[PACKAGE_DIR],
+    install_requires=REQUIRED,
+    extras_require=EXTRAS,
+    include_package_data=True,
+    classifiers=[
+        # Trove classifiers
+        # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+    ],
+    # $ setup.py publish support.
+    cmdclass={"upload": UploadCommand, "bumpversion": BumpVersionCommand},
+)
