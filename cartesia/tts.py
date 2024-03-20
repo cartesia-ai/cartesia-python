eb5f232c35c4d9d3d1a5723ca4a57c9ea58caa92
Timothy Luong <timothy.chongz.luong@gmail.com>
2024-03-20 10:17:06 -0700

    [CAR-328] Adding Voice Cloning to Python Client (#12)
    * Update CartesiaTTS api to support voice cloning and new api changes
    
    * Fixing the voice clone function
    
    * Removing a log, rerouting  to use
    
    * Fixing clip clone
    
    * Adding todo comment
    
    * Removing model_id, adding voice caching
    
    * Fixing typing
    
    * Updating Voice dict to map id instead of name
    
    * Changing clone_voice to return a dict
    
    ---------
    
    Co-authored-by: Arjun Desai <arjunaashna@gmail.com>

--- cartesia/tts.py
+++ cartesia/tts.py
@@ -1,7 +1,7 @@
 import base64
 import json
 import os
-from typing import List, TypedDict
+from typing import Dict, List, TypedDict, Union
 
 import numpy as np
 import requests
@@ -16,57 +16,126 @@ class AudioOutput(TypedDict):
     sampling_rate: int
 
 
+class VoiceOutput(TypedDict):
+    id: str
+    embedding: List[float]
+
+
 class CartesiaTTS:
     """The client for Cartesia's text-to-speech library."""
 
-    def __init__(self, *, model_id: str = None, api_key: str = None):
+    def __init__(self, *, api_key: str = None):
         """
         Args:
             api_key: The API key to use for authorization.
                 The api key is not currently enforced.
                 TODO: Set the API key.
         """
-        self.model_id = model_id or DEFAULT_MODEL_ID
         self.base_url = os.environ.get("CARTESIA_BASE_URL", DEFAULT_BASE_URL)
         self.api_key = api_key or os.environ.get("CARTESIA_API_KEY")
         self.api_version = os.environ.get("CARTESIA_API_VERSION", DEFAULT_API_VERSION)
         self.headers = {"X-API-Key": self.api_key, "Content-Type": "application/json"}
 
+        # Mapping from id -> embedding
+        self._voices: Dict[str, List[float]] = {}
+        self._downloaded_voices = False
+
     def models(self) -> List[str]:
         """Get a list of available models."""
         raise NotImplementedError()
 
-    def voices(self, model_id: str = None) -> List[str]:
+    def voices(self, *, refresh: bool = False) -> List[str]:
         """Returns a list of voices for a given model.
 
+        These voices can be used with :method:`CartesiaTTS.generate` to generate audio.
+
         Args:
-            model_id: The model to get voices for.
+            refresh: If ``True``, the API will be pinged if the voices for the model
+                are not already cached. Otherwise, default to the cached voices.
+
+        Returns:
+            List[str]: The list of voice ids for the model.
         """
-        model_id = model_id or self.model_id
-        response = requests.get(
-            f"{self._http_url()}/models/{model_id}/voices", headers=self.headers
-        )
+        if self._downloaded_voices and not refresh:
+            return self._voices
+
+        response = requests.get(f"{self._http_url()}/voices", headers=self.headers)
 
         if response.status_code != 200:
-            raise ValueError(f"Failed to get voices for model {model_id}. Error: {response.text}")
+            raise ValueError(f"Failed to get voices. Error: {response.text}")
 
-        return json.loads(response.text)
+        # Map from the table rows to a dict with key: "id" and value: "embedding"
+        # TODO: Remove the eval once the API returns a list of floats instead of a string
+        downloaded_voices = {voice["id"]: eval(voice["embedding"]) for voice in response.json()}
+        self._voices.update(downloaded_voices)
+        self._downloaded_voices = True
+
+        return self._voices
+
+    def clone_voice(self, *, filepath: str = None, link: str = None) -> VoiceOutput:
+        """Clone a voice from a filepath or YouTube url.
+
+        Args:
+            filepath: Path to audio file from which to get the audio.
+            link: The url to get the audio from. Currently only supports youtube shared urls.
+
+        Note:
+            Only one of `filepath` or `link` should be specified.
+
+        Note:
+            This voice will not persist over different clients. If you would like to reuse
+            the voice, you will have to save the voice embedding.
+
+        Note:
+            Voice cloning is only supported for the latest model.
+
+        Returns:
+            A dictionary containing:
+                * "id": The id of the cloned voice.
+                * "embedding": The embedding of the cloned voice.
+        """
+        if filepath and link:
+            raise ValueError("Only one of `filepath` or `url` should be specified.")
+
+        if filepath:
+            url = f"{self._http_url()}/voices/clone/clip"
+            files = {"clip": open(filepath, "rb")}
+            headers = self.headers.copy()
+            # The default content type of JSON is incorrect for file uploads
+            headers.pop("Content-Type")
+            response = requests.post(url, headers=headers, files=files)
+        else:
+            url = f"{self._http_url()}/voices/clone/url"
+            params = {"link": link}
+            response = requests.post(url, headers=self.headers, params=params)
+
+        if response.status_code != 200:
+            raise ValueError(
+                f"Failed to clone voice. Status Code: {response.status_code}\n"
+                f"Error: {response.text}"
+            )
+
+        # Handle successful response
+        out = response.json()
+        embedding = out["embedding"]
+        voice_id = out["id"]
+        self._voices[voice_id] = embedding
+
+        return {"id": voice_id, "embedding": embedding}
 
     def generate(
         self,
         *,
-        model_id: str = None,
         transcript: str,
         duration: int = None,
         chunk_time: float = None,
         lookahead: int = None,
-        voice: str = None,
+        voice: Union[str, List[float]] = None,
         stream: bool = False,
     ) -> AudioOutput:
         """Generate audio from a transcript.
 
         Args:
-            model_id: The model to use for generating audio.
             transcript: The text to generate audio for.
             duration: The maximum duration of the audio in seconds.
             chunk_time: How long each audio segment should be in seconds.
@@ -74,6 +143,7 @@ class CartesiaTTS:
             lookahead: The number of seconds to look ahead for each chunk.
                 This should not need to be adjusted.
             voice: The voice to use for generating audio.
+                This can either be a voice id (string) or an embedding vector (List[float]).
             stream: Whether to stream the audio or not.
                 If ``True`` this function returns a generator.
 
@@ -82,13 +152,10 @@ class CartesiaTTS:
                 * "audio": The audio as a 1D numpy array.
                 * "sampling_rate": The sampling rate of the audio.
         """
-        body = dict(
-            transcript=transcript,
-            model_id=model_id or DEFAULT_MODEL_ID,
-        )
+        body = dict(transcript=transcript, model_id=DEFAULT_MODEL_ID)
 
         if isinstance(voice, str):
-            voice = {"sources": [voice], "weights": [1.0]}
+            voice = self._voices[voice]
 
         optional_body = dict(
             duration=duration,
@@ -99,7 +166,10 @@ class CartesiaTTS:
         body.update({k: v for k, v in optional_body.items() if v is not None})
 
         response = requests.post(
-            f"{self._http_url()}/stream", stream=True, data=json.dumps(body), headers=self.headers
+            f"{self._http_url()}/audio/stream",
+            stream=True,
+            data=json.dumps(body),
+            headers=self.headers,
         )
         if response.status_code != 200:
             raise ValueError(f"Failed to generate audio. {response.text}")
