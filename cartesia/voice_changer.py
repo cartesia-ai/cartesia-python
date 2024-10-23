1681303e12a01d1febdc021a1ea0a19e75d99859
Noah Tye <noah.tye@cartesia.ai>
2024-10-23 11:18:34 -0700

    refactor change_voice_file and change_voice_bytes

--- cartesia/voice_changer.py
+++ cartesia/voice_changer.py
@@ -1,6 +1,6 @@
 import httpx
 
-from cartesia._types import OutputFormat
+from cartesia._types import StrictOutputFormat
 from cartesia.resource import Resource
 
 class VoiceChanger(Resource):
@@ -8,34 +8,15 @@ class VoiceChanger(Resource):
         self,
         input_path: str,
         voice_id: str,
-        output_format: OutputFormat,
+        output_format: StrictOutputFormat,
         output_path: str,
     ) -> None:
-        url = f"{self._http_url()}/voice-changer/bytes"
         with open(input_path, "rb") as input_file:
-            headers = self.headers.copy()
-            headers.pop("Content-Type", None)
-            files = {"clip": input_file}
-            data = {
-                "voice[id]": voice_id,
-                "output_format[container]": output_format["container"]
-						}
-            
-            if "encoding" in output_format:
-                data["output_format[encoding]"] = output_format["encoding"]
-            if "sample_rate" in output_format:
-                data["output_format[sample_rate]"] = output_format["sample_rate"]
-            if "bit_rate" in output_format:
-                data["output_format[bit_rate]"] = output_format["bit_rate"]
-
-            response = httpx.post(url, headers=headers, files=files, data=data, timeout=self.timeout)
-            if not response.is_success:
-                raise ValueError(f"Failed to change voice in clip. Error: {response.text}")
-
+            content = self.change_voice_bytes(input_file, voice_id, output_format)
         with open(output_path, "wb") as output_file:
-            output_file.write(response.content)
+            output_file.write(content)
 
-    def change_voice_bytes(self, audio_blob: bytes, voice_id: str, output_format: OutputFormat) -> bytes:
+    def change_voice_bytes(self, audio_blob: bytes, voice_id: str, output_format: StrictOutputFormat) -> bytes:
         url = f"{self._http_url()}/voice-changer/bytes"
         headers = self.headers.copy()
         headers.pop("Content-Type", None)
@@ -43,9 +24,14 @@ class VoiceChanger(Resource):
         data = {
             "voice[id]": voice_id,
             "output_format[container]": output_format["container"],
-            "output_format[encoding]": output_format["encoding"],
-            "output_format[sample_rate]": output_format["sample_rate"],
         }
+	
+        if "encoding" in output_format:
+            data["output_format[encoding]"] = output_format["encoding"]
+        if "sample_rate" in output_format:
+            data["output_format[sample_rate]"] = output_format["sample_rate"]
+        if "bit_rate" in output_format:
+            data["output_format[bit_rate]"] = output_format["bit_rate"]
 
         response = httpx.post(url, headers=headers, files=files, data=data, timeout=self.timeout)
         if not response.is_success:
